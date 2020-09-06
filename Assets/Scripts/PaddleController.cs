be0bb1548fddb023a6a22b4eb30ed0f20f858139
SauleStan <vogellietuva@gmail.com>
2020-09-06 15:58:33 +0300

    idk what these changes are and at this point i'm too afraid to ask

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -10,6 +10,9 @@ public class PaddleController : MonoBehaviour
     public Transform topTrans;
     public string inputAxis;
 
+    bool TouchingWalls;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +23,10 @@ public class PaddleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var test = new Vector3(-Input.GetAxis(inputAxis), 0, 0);
-        Debug.Log(test);
-        test = test * force * Time.deltaTime;
-        Debug.Log(test);
-        transform.Translate(test);
-        
+        var move = new Vector3(-Input.GetAxis(inputAxis), 0, 0);
+        move = move * force * Time.deltaTime;
+        Debug.Log(move);
+        transform.Translate(move);
+
     }
-}
+ }
